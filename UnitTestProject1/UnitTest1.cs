962d93a1c97f60f4e34318f61d7128d697e0f1be
Pavel Zlatarov <pzlatarov@uni-ruse.bg>
2022-12-01 11:03:19 +0200

    Unit Test

---
+++ UnitTestProject1/UnitTest1.cs
@@ -0,0 +1,28 @@
+﻿using GitUnitTestDemo;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace UnitTestProject1
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var math = new GitUnitTestDemo.Math(5, 2);
+            var math2 = new GitUnitTestDemo.Math(1234, 0);
+
+            Assert.AreEqual(7, math.Add());
+            Assert.AreEqual(3, math.Subtract());
+            Assert.AreEqual(10, math.Multiply());
+            Assert.AreEqual(2.5f, math.Divide());
+            Assert.AreEqual(Single.PositiveInfinity, math2.Divide());
+            //OR
+            Assert.IsTrue(Single.IsPositiveInfinity(math2.Divide()));
+            Assert.AreEqual(10, math2.SumDigits());
+            Assert.AreEqual("101", math.Binary());
+
+        }
+    }
+}
